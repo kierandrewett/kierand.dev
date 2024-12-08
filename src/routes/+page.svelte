a6a4da60c10fff1f696a220a1de2b2fb67fd3366
Kieran Drewett <kieran@dothq.org>
2024-12-08 05:05:21 +0000

    Fix capitalisation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
 <img class="profile-picture" src="/favicon.png" alt="Kieran Drewett Profile" />
 <h2>Kieran Drewett</h2>
 <p>
-	<span>Software engineer based in the UK.</span>
+	<span>Software Engineer based in the UK.</span>
         <br />
 	<span>Currently working on Dot Browser at <Link href="https://www.dothq.org/">Dot HQ</Link>.</span>
 </p>
